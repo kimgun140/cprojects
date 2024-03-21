5c5ef39bd9af740aaa159d8bbd420fe702500674
kimgun140 <kimgun140@gmail.com>
2024-03-21 21:34:09 +0900

    tt

--- C_Test/c0321.c
+++ C_Test/c0321.c
@@ -170,13 +170,119 @@
 
 //     return 0;
 // }
+// #include<stdio.h>
+// int main (void){
+    // int days[12] = {};
+    // //배열의 크기가 초기화 해준 값의 크기보다 작아서 오류발생한다.
+    // int sum = 0;
+    // int index ;
+    // for (index = 0; index <sizeof days/sizeof days[0]; index++ ){
+    //         // printf("%2d",days[index]);
+    //         days[index] = index;
+    //         printf("%3d",days[index]);
+
+    // }
+// int num1 = 15;
+// int num2 = 30;
+// int num3 = 45;
+// int num4 = 50;
+// int num5 = 100;
+
+// int* ptr1 = &num1;
+// int* ptr2 = &num2;
+// int* ptr3 = &num3;
+// int* ptr4 = &num4;
+// int* ptr5 = &num5;
+
+// ptr1 = ptr2;
+// ptr2 = ptr3;
+// ptr4 = ptr1;
+// ptr5 = ptr4;
+
+// *ptr3 *= 2;
+
+// printf("%d",ptr1);
+
+// }
+
+//p.474
+
+// 배열의 크기가 바뀌면 버그 생기니까 보통 배열의 크기는 상수선언을 해서
+// 상수로 사용한다.
+
+//페이지478
+//다차원 배열 타입 이름 [][]이부분은 리스트랑 비슷한듯?
+
+// rain.c -- 강우량 데이터로부터 각 해의 총 강우량과 몇년에 걸쳐 계산한 연평균 강우량,각달의 월 펼균 강우량을 구한다.
 #include<stdio.h>
-int main (void){
-    int days[] = {1,2,3,4,5,6,7,8,9};
-    //배열의 크기가 초기화 해준 값의 크기보다 작아서 오류발생한다.
-    int sum = 0;
-    int index ;
-    for (index = 0; index <sizeof days/sizeof days[0]; index++ ){
-            printf("%2d",days[index]);
+#define MONTHS 12
+#define YEARS 5
+int main(void){
+
+    //2010년에서 2014년까지의 강우량 디이터의 초기화
+    const float rain[YEARS][MONTHS] = 
+    {
+        {4.3,1.2,4.3,4.3,4.0,2.0,1.2,0.2,0.4,2.4,3.5,6.6},
+        {8.5,8.2,1.2,1.6,2.4,0.0,5.2,0.9,0.3,0.9,1.4,7.3},
+        {9.1,8.5,6.7,4.3,2.1,0.8,0.2,0.2,1.1,2.3,6.1,8.4},
+        {7.2,9.9,8.4,3.3,1.2,0.8,0.4,0.0,0.6,1.7,4.3,6.2},
+        {7.6,5.6,3.8,2.8,3.8,0.2,0.0,0.0,0.0,1.3,2.6,5.2}
+    };
+
+    int year, month;
+    float subtot, total;
+    float maxx = 0.0;
+
+    printf("년도 강우량(인치)\n");
+    for (year = 0, total = 0; year < YEARS; year++){
+        //각 해에 대해 12달치 강우량을 더한다.
+        for (month = 0, subtot = 0; month < MONTHS; month++)
+                subtot += rain[year][month];
+        printf("%5d %15.1f\n", 2010 + year, subtot);
+        total += subtot;
     }
+    printf("\n년 평균 강우량은 %.1f인치입니다.\n", total/YEARS);
+    printf("월편균 강우량은 다음과 같습니다. \n\n");
+    printf("Jan Feb Mar Apr May Jun Jul Aug Sep Oct");
+    printf("Nov Dec\n");
+
+    for (month = 0; month < MONTHS; month++){
+        for (year = 0,  subtot = 0; year< YEARS; year++)
+                subtot += rain[year][month];
+            printf("%4.1f", subtot/YEARS);
+    }
+    printf("\n");
+
+    // for (month = 0; month < MONTHS; month++){
+    //     for (year = 0, subtot = 0; year < YEARS; year++){
+    //     maxx = 0;
+
+    //             maxx = rain[year][month] >= rain[year][month+1] ? rain[year][month] : rain[year][month+1];
+    //     }
+                    
+    //                 printf("%.1f\n", maxx);
+                    
+                
+    //     // printf("%4.1f", subtot/YEARS);
+    // }
+        // for (year = 0, total = 0; year < YEARS; year++){
+        // //각 해에 대해 12달치 강우량을 더한다.
+        // for (month = 0, subtot = 0; month < MONTHS; month++)
+        //         maxx = rain[year][month] >= rain[year][month+1] ? rain[year][month] : rain[year][month+1];
+        // printf("%.1f\n", maxx);
+        // maxx = 0;
+
+        for (year = 0; year < YEARS; year++){
+        //각 해에 대해 12달치 강우량을 더한다.
+        for (month = 0; month < MONTHS; month++)
+                if (rain[year][month] >= maxx)
+                        maxx = rain[year][month];
+        printf("%.1f\n", maxx);
+        maxx = 0;
+
+    }
+    // printf("%.1f\n", maxx);
+    printf("\n");
+
+    return 0;
 }
